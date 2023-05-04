3da69a1ca77ab5658961643d4fb94dfcbb3be5e4
IvanPeM <ivan.pe.martinez@gmail.com>
2023-05-04 19:24:06 +0200

    commit

--- backend/database.js
+++ backend/database.js
@@ -4,10 +4,11 @@ const mongoose = require('mongoose');
 
 // para inicializar la basa de datos se tiene que poner
 // en la terminal mongod.
-mongoose.connect('mongodb://localhost:27017/', {
-    useCreateIndex: true,
+mongoose.connect('mongodb://localhost:27017/Restaurante', {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useUnifiedTopology: true,
+    createIndex: true,
+    findOneAndUpdate: false
 }).then(() => {
     console.log('Conexión existosa a la base de datos.');
 }).catch(err => {
