e6b0a4f6a6c55aab5144f457aa1d2c959a742091
IvanPeM <ivan.pe.martinez@gmail.com>
2023-05-06 18:16:06 +0200

    commit

--- backend/database.js
+++ backend/database.js
@@ -7,7 +7,8 @@ const Plato = require('./model/Plato.model.js');
 const Mesa = require('./model/Mesa.model.js');
 
 // La URL de la base de datos
-const DBURL = process.env.MONDODB_URL;
+const DBURL = process.env.MONGODB_URL;
+console.log(DBURL);
 
 // Me conencto a la base de datos.
 mongoose.connect(DBURL, {
@@ -17,13 +18,13 @@ mongoose.connect(DBURL, {
 ).catch(err => console.error(err));
 
 // Prueba para ver como funciona añadir un nuevo plato.
-const pruebaPlato = new Plato({
-    nombre: 'prueba',
-    numero: 1,
-    foto: 'plato01',
-    precio: 0,
-    ingredientes: ['Salmón','Arroz']
-});
+// const pruebaPlato = new Plato({
+//     nombre: 'prueba',
+//     numero: 1,
+//     foto: 'plato01',
+//     precio: 0,
+//     ingredientes: ['Salmón','Arroz']
+// });
 
 // Guardar en la base de datos el plato de prueba.
 // pruebaPlato.save()
@@ -35,6 +36,6 @@ const pruebaPlato = new Plato({
 //     });
 
 // Buscar en la base de datos todos los Platos.
-Plato.find({}).then(result =>{
-    console.log(result);
-});
+// Plato.find({}).then(result =>{
+//     console.log(result);
+// });
