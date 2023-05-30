4d5d017c43673ab0ef3e13f459b22cf813ee7bc2
IvanPeM <ivan.pe.martinez@gmail.com>
2023-05-30 21:17:36 +0200

    commit

--- frontend/public/menu.js
+++ frontend/public/menu.js
@@ -1,5 +1,35 @@
 "use strict";
 
+let dinero = 0;
+
 function pedir(ultimo){
-    console.log(ultimo);
+    let platos = {};
+    for (let i = ultimo; i > 0; i--) {
+        platos[i] = document.getElementById(`plato${i}`).value
+    }
+    console.log(platos);
+}
+
+function menos(numero){
+    let precio = document.getElementById(`precio${numero}`);
+    let valor = parseInt(precio.innerText);
+    if(document.getElementById(`plato${numero}`).value != 0){
+        if(dinero != 0){
+            dinero -= valor;
+            let pedir = document.getElementById(`pedir`);
+            if(dinero == 0){
+                pedir.innerHTML = `Pedir`;
+            }else{
+                pedir.innerHTML = `Pedir ${dinero}€`;
+            }
+        }
+    }
+}
+
+function mas(numero){
+    let precio = document.getElementById(`precio${numero}`);
+    let valor = parseInt(precio.innerText);
+    dinero += valor;
+    let pedir = document.getElementById(`pedir`);
+    pedir.innerHTML = `Pedir ${dinero}€`;
 }
